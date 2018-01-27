d7505636d4a6c304424477ef05223cdb6b51ce0e
Hendi Santika <hendisantika@gmail.com>
2018-01-27 19:08:37 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootmysqlrestapi/SpringBootMysqlRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootmysqlrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMysqlRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMysqlRestApiApplication.class, args);
+    }
+}
