27634466c25fdc288b1b613a22b51f9074d8f48a
hendisantika <hendisantika@yahoo.co.id>
2024-07-09 07:11:46 +0700

    Bug Fix Test 🫸🌀✏️📗 :octocat:🐧🐳⬆

--- src/main/java/com/example/easynotes/model/Note.java
+++ src/main/java/com/example/easynotes/model/Note.java
@@ -1,12 +1,20 @@
 package com.example.easynotes.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import java.util.Date;
 
 /**
