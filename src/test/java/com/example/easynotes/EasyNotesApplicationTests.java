5c4cf7e9e75afb2e551e9ed7b37deeef72d6c615
hendisantika <hendisantika@yahoo.co.id>
2024-07-09 07:17:36 +0700

    Add TableTestAssertion 🫸🌀✏️📗:octocat:🐧🐳⬆

--- src/test/java/com/example/easynotes/EasyNotesApplicationTests.java
+++ src/test/java/com/example/easynotes/EasyNotesApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.easynotes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 public class EasyNotesApplicationTests {
 
     @Test
